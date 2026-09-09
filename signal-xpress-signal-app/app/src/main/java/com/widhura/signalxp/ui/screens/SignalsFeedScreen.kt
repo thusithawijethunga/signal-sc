033abd5ebb7dc2dd2f411044afbfffd56492505e
Thusitha Wijethunga <thusithawijethunga@gmail.com>
2026-09-09 15:02:53 +0530

    signal socket

--- signal-xpress-signal-app/app/src/main/java/com/widhura/signalxp/ui/screens/SignalsFeedScreen.kt
+++ signal-xpress-signal-app/app/src/main/java/com/widhura/signalxp/ui/screens/SignalsFeedScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
